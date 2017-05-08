244e1ad7cc0a57aedead31198f3b8fbd9e7214f0
Mateusz <mateuszmonas@gmail.com>
2017-05-08 09:20:42 +0200

    code improvements

--- app/src/main/java/com/gmail/mateuszmonas/macroapp/faktura/FakturaContract.java
+++ app/src/main/java/com/gmail/mateuszmonas/macroapp/faktura/FakturaContract.java
@@ -4,7 +4,7 @@ package com.gmail.mateuszmonas.macroapp.faktura;
 import com.gmail.mateuszmonas.macroapp.BasePresenter;
 import com.gmail.mateuszmonas.macroapp.BaseView;
 
-public class FakturaContract {
+interface FakturaContract {
 
     interface View extends BaseView<Presenter> {
     }
