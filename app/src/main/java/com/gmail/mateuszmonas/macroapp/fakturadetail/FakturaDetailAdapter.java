b27e57d095853513f623b7b3eaf3e74ce3ccf9f5
Mateusz <mateuszmonas@gmail.com>
2017-05-15 13:00:22 +0200

    test

--- app/src/main/java/com/gmail/mateuszmonas/macroapp/fakturadetail/FakturaDetailAdapter.java
+++ app/src/main/java/com/gmail/mateuszmonas/macroapp/fakturadetail/FakturaDetailAdapter.java
@@ -1,5 +1,92 @@
 package com.gmail.mateuszmonas.macroapp.fakturadetail;
 
 
-public class FakturaDetailAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gmail.mateuszmonas.macroapp.R;
+import com.gmail.mateuszmonas.macroapp.data.PozycjaFaktury;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FakturaDetailAdapter extends RecyclerView.Adapter<FakturaDetailAdapter.ViewHolder> {
+
+    private List<PozycjaFaktury> pozycjeFaktury;
+
+    public FakturaDetailAdapter(List<PozycjaFaktury> pozycjeFaktury) {
+        this.pozycjeFaktury = pozycjeFaktury;
+        //add header
+        this.pozycjeFaktury.add(0, new PozycjaFaktury());
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pozycja_faktury_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if(position == 0){
+            PozycjaFaktury pozycjaFaktury = pozycjeFaktury.get(position);
+            holder.lp.setText("Lp.");
+            holder.nazwa.setText("Nazwa towaru lub usługi");
+            holder.ilosc.setText("ilość");
+            holder.jednostka.setText("JM");
+            holder.cenaNetto.setText("C. nett");
+            holder.wartNetto.setText("W. netto");
+            holder.wartVAT.setText("VAT");
+            holder.wartBrutto.setText("W. brutto");
+        }else {
+            PozycjaFaktury pozycjaFaktury = pozycjeFaktury.get(position);
+            holder.lp.setText(String.format(Locale.getDefault(), "%d", pozycjaFaktury.getPozycja()));
+            holder.nazwa.setText(pozycjaFaktury.getNazwa());
+            holder.ilosc.setText(String.format(Locale.getDefault(), "%d", pozycjaFaktury.getIlosc()));
+            holder.jednostka.setText(pozycjaFaktury.getJednostka());
+            holder.cenaNetto.setText(new DecimalFormat("#.00").format(pozycjaFaktury.getCenaNetto()) + "PLN");
+            holder.wartNetto.setText(new DecimalFormat("#.00").format(pozycjaFaktury.getWartoscNetto()) + "PLN");
+            holder.wartVAT.setText(new DecimalFormat("#.00").format(pozycjaFaktury.getWartoscVat()) + "PLN");
+            holder.wartBrutto.setText(new DecimalFormat("#.00").format(pozycjaFaktury.getWartoscBrutto()) + "PLN");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return pozycjeFaktury.size();
+    }
+
+    void replaceData(List<PozycjaFaktury> pozycjeFaktury){
+        setList(pozycjeFaktury);
+        notifyDataSetChanged();
+    }
+
+    private void setList(List<PozycjaFaktury> pozycjeFaktury){
+        this.pozycjeFaktury = pozycjeFaktury;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        final View view;
+        @BindView(R.id.lp)TextView lp;
+        @BindView(R.id.nazwa)TextView nazwa;
+        @BindView(R.id.ilosc)TextView ilosc;
+        @BindView(R.id.jednostka)TextView jednostka;
+        @BindView(R.id.cenaNetto)TextView cenaNetto;
+        @BindView(R.id.wartNetto)TextView wartNetto;
+        @BindView(R.id.wartVAT)TextView wartVAT;
+        @BindView(R.id.wartBrutto)TextView wartBrutto;
+        public ViewHolder(View view) {
+            super(view);
+            this.view =  view;
+            ButterKnife.bind(this, view);
+        }
+    }
+
 }
--- app/src/main/java/com/gmail/mateuszmonas/macroapp/fakturadetail/FakturaDetailFragment.java
+++ app/src/main/java/com/gmail/mateuszmonas/macroapp/fakturadetail/FakturaDetailFragment.java
@@ -3,6 +3,8 @@ package com.gmail.mateuszmonas.macroapp.fakturadetail;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,12 @@ import android.widget.TextView;
 import com.gmail.mateuszmonas.macroapp.R;
 import com.gmail.mateuszmonas.macroapp.data.PozycjaFaktury;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class FakturaDetailFragment extends Fragment implements FakturaDetailContract.View {
 
@@ -23,6 +28,10 @@ public class FakturaDetailFragment extends Fragment implements FakturaDetailCont
     TextView brakPolaczenia;
     @BindView(R.id.loader)
     ProgressBar loader;
+    @BindView(R.id.fakturaDetailRecyclerView)
+    RecyclerView recyclerView;
+    FakturaDetailAdapter adapter;
+    Unbinder unbinder;
 
     public FakturaDetailFragment() {
         // Required empty public constructor
@@ -38,20 +47,38 @@ public class FakturaDetailFragment extends Fragment implements FakturaDetailCont
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ArrayList<PozycjaFaktury> a = new ArrayList<>();
+        for(int i = 0 ; i<3 ; i++) {
+            PozycjaFaktury pozycjaFaktury = new PozycjaFaktury();
+            pozycjaFaktury.setIlosc(2);
+            pozycjaFaktury.setCenaNetto(65.89);
+            pozycjaFaktury.setJednostka("sztuka");
+            pozycjaFaktury.setNazwa("laptop");
+            pozycjaFaktury.setPozycja(i);
+            pozycjaFaktury.setWartoscBrutto(142.32);
+            pozycjaFaktury.setWartoscNetto(131.78);
+            pozycjaFaktury.setWartoscVat(10.54);
+            a.add(pozycjaFaktury);
+        }
+        adapter=new FakturaDetailAdapter(a);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faktura_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_faktura_detail, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
+        return view;
     }
-
 
     @Override
     public void showFakturaDetails(List<PozycjaFaktury> pozycje) {
 
     }
+
     @Override
     public void showBrakPolaczenia() {
         if(getView() != null) {
@@ -61,20 +88,23 @@ public class FakturaDetailFragment extends Fragment implements FakturaDetailCont
 
     @Override
     public void showLoadingView() {
-        if(getView()!=null)
+        if(getView()!=null) {
             loader.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     public void hideLoadingView() {
-        if(getView()!=null)
+        if(getView()!=null) {
             loader.setVisibility(View.GONE);
+        }
     }
 
     @Override
     public void hideBrakPolaczenia() {
-        if(getView()!=null)
+        if(getView()!=null) {
             brakPolaczenia.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -85,6 +115,12 @@ public class FakturaDetailFragment extends Fragment implements FakturaDetailCont
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 
     @Override
